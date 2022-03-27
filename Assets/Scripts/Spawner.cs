e21602183e38e425547dd34141785c9c4155e4a2
ildar v <genanapolotence@gmail.com>
2022-03-27 11:20:43 +0500

    27.03
    11.20

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Coin _template;
+    [SerializeField] private Transform _coinPnoints;
+
+    private Transform[] _points;
+
+    private void Start()
+    {
+        _points = new Transform[_coinPnoints.childCount];
+
+        for (int i = 0; i < _coinPnoints.childCount; i++)
+        {
+            _points[i] = _coinPnoints.GetChild(i);
+        }
+
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        foreach (var point in _points)
+        {
+            Instantiate(_template, point.position, Quaternion.identity);
+        }        
+    }
+}
