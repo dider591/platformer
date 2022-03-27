86d6558977b77e26af44dd506ff984ad55ae5920
ildar v <genanapolotence@gmail.com>
2022-03-27 10:54:30 +0500

    27.03
    10.54

---
+++ Assets/Scripts/CoinTake.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinTake : MonoBehaviour
+{
+    private int coinCount = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            coinCount++;
+            Destroy(gameObject);
+        }
+    }
+}
